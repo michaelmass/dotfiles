91e6ee9131dc06782a00e21fa59a022f96cf734f
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2024-01-13 23:05:31 -0500

    fix(install): adding scoop buckets in the install.ps1 script (#39)

--- install.ps1
+++ install.ps1
@@ -4,12 +4,15 @@ if (Get-Command scoop -errorAction SilentlyContinue) {
   iwr -useb get.scoop.sh | iex
 }
 
-scoop install git
 scoop bucket add extras
+scoop bucket add versions
+scoop bucket add volllly https://github.com/volllly/scoop-bucket
+
+scoop install git
 scoop install vcredist2022
 scoop install sudo
-scoop bucket add volllly https://github.com/volllly/scoop-bucket
 scoop install volllly/rotz
+
 cd $env:USERPROFILE
 
 if (Test-Path ./.dotfiles) {
