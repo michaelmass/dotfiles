529ce91b6230e60cdfe911a47b7cf339ef63fa5b
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2025-06-17 00:25:41 -0400

    fix(finicky): update work URLs (#197)

--- finicky/finicky.js
+++ finicky/finicky.js
@@ -16,31 +16,52 @@ const workBrowser = {
 };
 
 const workUrls = [
-  "botpress.productboard.com",
-  "botpress.grafana.net",
-  "botpress-rm.sentry.io",
-  "botpresshq.slack.com",
-  "botpress-aws.awsapps.com",
-  "device.sso.us-east-1.amazonaws.com",
   "app.botpress.cloud",
   "app.botpress.dev",
-  "app.krisp.ai",
+  "botpress-aws.awsapps.com",
+  "botpress-rm.sentry.io",
   "botpress.bamboohr.com",
-  "meet.google.com",
-  "linear.app/botpress",
-  "google.zoom.us",
+  "botpress.grafana.net",
+  "botpress.pagerduty.com",
+  "botpress.productboard.com",
+  "botpress.retool.com",
+  "botpresshq.slack.com",
   "sauron.botpress.cloud",
   "sauron.botpress.dev",
-  "www.cursor.com",
-  "botpress.retool.com",
+  "app.krisp.ai",
   "cursor.com",
+  "device.sso.us-east-1.amazonaws.com",
+  "google.zoom.us",
+  "meet.google.com",
+  "www.cursor.com",
+  "us-east-1.console.aws.amazon.com",
 ];
 
 const workOrgUrls = {
-  "vercel.com": "botpress-team",
-  "supabase.com": "zxuehanuqnemevxljlpy",
-  "github.com": "/botpress/",
   "cloud.digitalocean.com": "e869a5",
+  "console.anthropic.com": "#botpress",
+  "dashboard.heroku.com": "/botpress-internal/",
+  "dnsimple.com": "/78774/",
+  "github.com": "/botpress/",
+  "insights.hotjar.com": "#botpress",
+  "linear.app": "/botpress/",
+  "supabase.com": "zxuehanuqnemevxljlpy",
+  "trello.com": "/botpress1/",
+  "vercel.com": "botpress-team",
+  "platform.openai.com": "#botpress",
+  "huggingface.co": "/botpress-hq/",
+  "admin.atlassian.com": [
+    "907bakbd-da41-19ab-k502-2c857bj2kd6b",
+    "d58c9551-127c-475c-a274-1b645601c099",
+  ],
+  "grafana.com": "botpress",
+  "dash.cloudflare.com": "1bf6525091ec55ebe4935a38ab001420",
+  "app.segment.com": "/botpress-production/",
+  "mixpanel.com": "2313408",
+  "depot.dev": "w8z3zskrwb",
+  "manage.statuspage.io": "dxgcnnf1xq2r",
+  "miro.com": ["3458764561695270325", "3458764558992614189"],
+  "console.upstash.com": "fe84ae13-9b27-4071-bae8-3157c5f8623c",
 };
 
 const workUrl = ({ url }) => {
@@ -52,11 +73,20 @@ const workUrl = ({ url }) => {
     return true;
   }
 
-  if (workOrgUrls[url.host]) {
-    const fullpath = url.pathname + url.hash + url.search;
+  const org = workOrgUrls[url.host];
 
-    if (fullpath.includes(workOrgUrls[url.host])) {
-      return true;
+  if (org) {
+    const fullpath =
+      url.pathname + (url.hash ? `#${url.hash}` : "") + url.search;
+
+    const orgs = typeof org === "string" ? [org] : org;
+
+    for (const org of orgs) {
+      console.log(fullpath, org);
+
+      if (fullpath.includes(org)) {
+        return true;
+      }
     }
   }
 
