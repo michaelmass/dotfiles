ec1f0ab8d012bc077c9538129451dbe116ded2c5
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2025-07-16 09:44:03 -0400

    feat(finicky): update configuration with new work URLs and improved organization (#210)

--- finicky/finicky.js
+++ finicky/finicky.js
@@ -18,6 +18,8 @@ const workBrowser = {
 const workUrls = [
   "app.botpress.cloud",
   "app.botpress.dev",
+  "app.floatfinancial.com",
+  "app.krisp.ai",
   "botpress-aws.awsapps.com",
   "botpress-rm.sentry.io",
   "botpress.bamboohr.com",
@@ -26,45 +28,55 @@ const workUrls = [
   "botpress.productboard.com",
   "botpress.retool.com",
   "botpresshq.slack.com",
+  "cursor.com",
+  "dashboard.ngrok.com",
+  "device.sso.us-east-1.amazonaws.com",
+  "google.zoom.us",
+  "meet.google.com",
   "sauron.botpress.cloud",
   "sauron.botpress.dev",
   "studio.botpress.cloud",
   "studio.botpress.dev",
-  "app.krisp.ai",
-  "cursor.com",
-  "device.sso.us-east-1.amazonaws.com",
-  "google.zoom.us",
-  "meet.google.com",
-  "www.cursor.com",
   "us-east-1.console.aws.amazon.com",
-  "app.floatfinancial.com",
+  "www.cursor.com",
 ];
 
 const workOrgUrls = {
-  "cloud.digitalocean.com": "e869a5",
-  "console.anthropic.com": "#botpress",
-  "dashboard.heroku.com": "/botpress-internal/",
-  "dnsimple.com": "/78774/",
-  "github.com": "/botpress/",
-  "insights.hotjar.com": "#botpress",
-  "linear.app": "/botpress/",
-  "supabase.com": "zxuehanuqnemevxljlpy",
-  "trello.com": "/botpress1/",
-  "vercel.com": "botpress-team",
-  "platform.openai.com": "#botpress",
-  "huggingface.co": "/botpress-hq/",
   "admin.atlassian.com": [
     "907bakbd-da41-19ab-k502-2c857bj2kd6b",
     "d58c9551-127c-475c-a274-1b645601c099",
   ],
-  "grafana.com": "botpress",
-  "dash.cloudflare.com": "1bf6525091ec55ebe4935a38ab001420",
+  "app.docker.com": "botpress",
   "app.segment.com": "/botpress-production/",
-  "mixpanel.com": "2313408",
-  "depot.dev": "w8z3zskrwb",
-  "manage.statuspage.io": "dxgcnnf1xq2r",
-  "miro.com": ["3458764561695270325", "3458764558992614189"],
+  "cloud.digitalocean.com": "e869a5",
+  "console.anthropic.com": "#botpress",
+  "console.cloud.google.com": "782387313636",
   "console.upstash.com": "fe84ae13-9b27-4071-bae8-3157c5f8623c",
+  "dash.cloudflare.com": "1bf6525091ec55ebe4935a38ab001420",
+  "dashboard.heroku.com": "/botpress-internal/",
+  "depot.dev": "w8z3zskrwb",
+  "dnsimple.com": "/78774/",
+  "github.com": "/botpress/",
+  "grafana.com": "botpress",
+  "groups.google.com": "botpress.com",
+  "huggingface.co": "/botpress-hq/",
+  "insights.hotjar.com": "#botpress",
+  "linear.app": "/botpress/",
+  "manage.statuspage.io": "dxgcnnf1xq2r",
+  "miro.com": [
+    "3458764561695270325",
+    "3458764558992614189",
+    "3458764559398815584",
+  ],
+  "mixpanel.com": "2313408",
+  "platform.openai.com": "#botpress",
+  "supabase.com": "zxuehanuqnemevxljlpy",
+  "trello.com": "/botpress1/",
+  "vercel.com": "botpress-team",
+  "www.figma.com": "1086003688753602071",
+  "zapier.com": ["3761911"],
+  "developer.zapier.com": "179950",
+  "healthchecks.io": "016ab399-0bcf-4f0f-a7fe-0ae299edb1d6",
 };
 
 const workUrl = ({ url }) => {
@@ -76,12 +88,15 @@ const workUrl = ({ url }) => {
     return true;
   }
 
+  const fullpath = url.pathname + (url.hash ? `#${url.hash}` : "") + url.search;
+
+  if (fullpath.includes("#botpress")) {
+    return true;
+  }
+
   const org = workOrgUrls[url.host];
 
   if (org) {
-    const fullpath =
-      url.pathname + (url.hash ? `#${url.hash}` : "") + url.search;
-
     const orgs = typeof org === "string" ? [org] : org;
 
     for (const org of orgs) {
