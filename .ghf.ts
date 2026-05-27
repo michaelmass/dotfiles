efb3d6610202e7b6b0ad1c1b914dd0d12d720bf5
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2026-05-26 23:53:10 -0400

    chore(config): update .ghf config URL, fix PATH imports, add .cachebro to .gitignore, extend Tiltfile services, and adjust PNPM path handling (#286)

--- .ghf.ts
+++ .ghf.ts
@@ -1,11 +1,5 @@
 import type { Config } from './.ghf.type'
 
 export default {
-  extends: ['https://michaelmass.github.io/ghf/ghf.default.json'],
-  rules: [
-    {
-      type: 'delete',
-      path: 'biome.json',
-    },
-  ],
+  extends: ['https://michaelmass.github.io/ghf/ghf/default.json'],
 } satisfies Config
